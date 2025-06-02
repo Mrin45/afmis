95e0493dce48cca966331f0ce80bdbff507fc557
Mrinmoy Nath <86364520+Mrin45@users.noreply.github.com>
2025-06-03 02:05:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -449,9 +449,9 @@ app = Flask(__name__)
 
 
 # Get the port from the environment variable
-# port = int(os.environ.get("PORT", 5000))
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+port = int(os.environ.get("PORT", 5000))
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
 
 
 # Define routes
